3ad4cf21a88c43c143a784d963b3aac4e7c71d81
Dan <dan@ekwholesale.co.uk>
2019-09-19 19:55:59 +0100

    added graphql to package.json

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const Gql = require('graphql');
 const client = new Discord.Client();
 require('dotenv').config();
 
@@ -6,10 +7,10 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
-  }
-});
+// client.on('message', msg => {
+//   if (msg.content === 'ping') {
+//     msg.reply('pong');
+//   }
+// });
 
 client.login(process.env.BOT_SECRET);
